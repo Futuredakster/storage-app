43535942f91d8f629234bf37b2339b4c6d37c91d
Futuredakster <danny.kaikov.m@gmail.com>
2025-06-16 16:59:14 -0700

    first commit

---
+++ eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+    "next/core-web-vitals"
+  ],
+  plugins: ["tailwindcss"],
+  rules: {
+    // optional custom rules
+  }
+};
